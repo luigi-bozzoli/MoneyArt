5989c4788c52724222b8801ece2f81290b4bccb8
Xzeni <xzenitz@gmail.com>
2021-12-30 23:28:40 +0100

    Updated Partecipazione and Rivendita Beans

--- src/main/java/it/unisa/c02/moneyart/model/beans/Rivendita.java
+++ src/main/java/it/unisa/c02/moneyart/model/beans/Rivendita.java
@@ -1,2 +1,102 @@
-package it.unisa.c02.moneyart.model.beans;public class Rivendita {
+package it.unisa.c02.moneyart.model.beans;
+
+
+/**
+ * Questa classe rappresenta una Rivendita.
+ * Una segnalazione è caratterizzata da: id (autogenerato dal DB),
+ * idOpera (identificativo dell'opera che è stata rivenduta),
+ * stato e prezzo.
+ */
+public class Rivendita {
+
+    /**
+     * Costruttore dell classe Rivendita.
+     *
+     * @param idOpera identificativo dell'asta a cui l'utente ha partecipato
+     * @param stato identificativo dell'asta a cui l'utente ha partecipato
+     * @param prezzo identificativo dell'utente che ha partecipato all'asta
+     */
+    public Rivendita(int idOpera, String stato, double prezzo) {
+        this.id = Rivendita.NO_ID;
+        this.idOpera = idOpera;
+        this.stato = stato;
+        this.prezzo = prezzo;
+    }
+
+    /**
+     * Restituisce l'identificativo della rivendita.
+     *
+     * @return identificativo della rivendita
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Imposta l'identificativo della rivendita.
+     *
+     * @param id identificativo della rivendita
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Restituisce l'identificativo dell'asta a cui l'utente ha partecipato.
+     *
+     * @return identificativo dell'asta a cui l'utente ha partecipato
+     */
+    public int getIdOpera() {
+        return idOpera;
+    }
+
+    /**
+     * Imposta l'identificativo dell'opera rivenduta.
+     *
+     * @param idOpera identificativo dell'opera rivenduta
+     */
+    public void setIdOpera(int idOpera) { this.idOpera = idOpera;}
+
+    /**
+     * Restituisce lo stato della rivendita.
+     *
+     * @return stato della rivendita
+     */
+    public String getStato() {
+        return stato;
+    }
+
+    /**
+     * Imposta lo stato della rivendita
+     *
+     * @param stato stato della rivendita
+     */
+    public void setStato(String stato) {
+        this.stato = stato;
+    }
+
+    /**
+     * Restituisce il valore del prezzo a cui l'opera è stata rivenduta.
+     *
+     * @return il valore del prezzo a cui l'opera è stata rivenduta
+     */
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    /**
+     * Imposta il prezzo della rivendita.
+     *
+     * @param prezzo prezzo a cui l'opera è stata rivenduta
+     */
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public static final int NO_ID = -1;
+
+    private int id;
+    private int idOpera;
+    private String stato;
+    private double prezzo;
 }
