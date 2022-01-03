9720881e72f0f7a9f70ff853625a8394421159dd
shoyll <michaeldesantis98@gmail.com>
2022-01-03 21:46:33 +0100

    UtenteDaoImpl's methods update

--- src/main/java/it/unisa/c02/moneyart/model/dao/interfaces/UtenteDAO.java
+++ src/main/java/it/unisa/c02/moneyart/model/dao/interfaces/UtenteDAO.java
@@ -1,6 +1,15 @@
 package it.unisa.c02.moneyart.model.dao.interfaces;
 
+import it.unisa.c02.moneyart.model.beans.Asta;
+import it.unisa.c02.moneyart.model.beans.Notifica;
+import it.unisa.c02.moneyart.model.beans.Rivendita;
 import it.unisa.c02.moneyart.model.beans.Utente;
 
+import java.util.List;
+
+/**
+ * Questa classe rappresenta il DAO di un Utente.
+ */
 public interface UtenteDAO extends GenericDao<Utente> {
+//signature metodi aggiuntivi.....
 }
