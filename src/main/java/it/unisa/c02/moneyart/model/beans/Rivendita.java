04e13578db9d478ab5b9fcc9a6c9f3a3832db696
Xzeni <xzenitz@gmail.com>
2022-01-03 23:38:18 +0100

    Updated Rivendita Bean and RivenditaDaoImpl (Checkstyle)

--- src/main/java/it/unisa/c02/moneyart/model/beans/Rivendita.java
+++ src/main/java/it/unisa/c02/moneyart/model/beans/Rivendita.java
@@ -9,113 +9,115 @@ package it.unisa.c02.moneyart.model.beans;
  */
 public class Rivendita {
 
-    /**
-     * Costruttore dell classe Rivendita.
-     *
-     * @param opera opera a cui la rivendita fa riferimento
-     * @param stato stato della rivendita
-     * @param prezzo prezzo dell'opera rivenduta
-     */
-    public Rivendita(Opera opera, Stato stato, double prezzo) {
-        this.id = Rivendita.NO_ID;
-        this.Opera = opera;
-        this.stato = stato;
-        this.prezzo = prezzo;
-    }
+  /**
+   * Costruttore dell classe Rivendita.
+   *
+   * @param opera opera a cui la rivendita fa riferimento
+   * @param stato stato della rivendita
+   * @param prezzo prezzo dell'opera rivenduta
+   */
+  public Rivendita(Opera opera, Stato stato, double prezzo) {
+    this.id = Rivendita.NO_ID;
+    this.opera = opera;
+    this.stato = stato;
+    this.prezzo = prezzo;
+  }
 
-    /**
-     * Costruttore vuoto della classe Rivendita.
-     */
-    public Rivendita() {
-    }
+  /**
+   * Costruttore vuoto della classe Rivendita.
+   */
+  public Rivendita() {
+  }
 
-    /**
-     * Restituisce l'identificativo della rivendita.
-     *
-     * @return identificativo della rivendita
-     */
-    public int getId() {
-        return id;
-    }
+  /**
+   * Restituisce l'identificativo della rivendita.
+   *
+   * @return identificativo della rivendita
+   */
+  public int getId() {
+    return id;
+  }
 
-    /**
-     * Imposta l'identificativo della rivendita.
-     *
-     * @param id identificativo della rivendita
-     */
-    public void setId(int id) {
-        this.id = id;
-    }
+  /**
+   * Imposta l'identificativo della rivendita.
+   *
+   * @param id identificativo della rivendita
+   */
+  public void setId(int id) {
+    this.id = id;
+  }
 
-    /**
-     * Restituisce l'opera rivenduta
-     *
-     * @return opera rivenduta
-     */
-    public Opera getOpera() {
-        return Opera;
-    }
+  /**
+   * Restituisce l'opera rivenduta.
+   *
+   * @return opera rivenduta
+   */
+  public Opera getOpera() {
+    return opera;
+  }
 
-    /**
-     * Imposta l'opera rivenduta.
-     *
-     * @param opera opera rivenduta
-     */
-    public void setOpera(Opera opera) { this.Opera = opera;}
+  /**
+   * Imposta l'opera rivenduta.
+   *
+   * @param opera opera rivenduta
+   */
+  public void setOpera(Opera opera) {
+    this.opera = opera;
+  }
 
-    /**
-     * Restituisce lo stato della rivendita.
-     *
-     * @return stato della rivendita
-     */
-    public Stato getStato() {
-        return stato;
-    }
+  /**
+   * Restituisce lo stato della rivendita.
+   *
+   * @return stato della rivendita
+   */
+  public Stato getStato() {
+    return stato;
+  }
 
-    /**
-     * Imposta lo stato della rivendita
-     *
-     * @param stato stato della rivendita
-     */
-    public void setStato(Stato stato) {
-        this.stato = stato;
-    }
+  /**
+   * Imposta lo stato della rivendita.
+   *
+   * @param stato stato della rivendita
+   */
+  public void setStato(Stato stato) {
+    this.stato = stato;
+  }
 
-    /**
-     * Restituisce il valore del prezzo a cui l'opera è stata rivenduta.
-     *
-     * @return il valore del prezzo a cui l'opera è stata rivenduta
-     */
-    public double getPrezzo() {
-        return prezzo;
-    }
+  /**
+   * Restituisce il valore del prezzo a cui l'opera è stata rivenduta.
+   *
+   * @return il valore del prezzo a cui l'opera è stata rivenduta
+   */
+  public double getPrezzo() {
+    return prezzo;
+  }
 
-    /**
-     * Imposta il prezzo della rivendita.
-     *
-     * @param prezzo prezzo a cui l'opera viene rivenduta
-     */
-    public void setPrezzo(double prezzo) {
-        this.prezzo = prezzo;
-    }
+  /**
+   * Imposta il prezzo della rivendita.
+   *
+   * @param prezzo prezzo a cui l'opera viene rivenduta
+   */
+  public void setPrezzo(double prezzo) {
+    this.prezzo = prezzo;
+  }
 
-    private int id;
-    private Opera Opera;
-    private Stato stato;
-    private double prezzo;
+  private int id;
+  private Opera opera;
+  private Stato stato;
+  private double prezzo;
 
-    /**
-     * ENUM che rappresenta i possibili stati della rivendita.
-     * IN_CORSO: la rivendita è in corso
-     * TERMINATA: la rivendita è terminata
-     */
-    public enum Stato {
-        IN_CORSO, TERMINATA
-    }
+  /**
+   * ENUM che rappresenta i possibili stati della rivendita.
+   * IN_CORSO: la rivendita è in corso
+   * TERMINATA: la rivendita è terminata
+   */
+  public enum Stato {
+    IN_CORSO, TERMINATA
+  }
 
 
-    /**
-     * costante per segnalare la mancanza di identificatore.
-     */
-    public static final int NO_ID = -1;
+  /**
+   * costante per segnalare la mancanza di identificatore.
+   */
+  public static final int NO_ID = -1;
 }
--- src/main/java/it/unisa/c02/moneyart/model/dao/RivenditaDaoImpl.java
+++ src/main/java/it/unisa/c02/moneyart/model/dao/RivenditaDaoImpl.java
@@ -1,12 +1,17 @@
 package it.unisa.c02.moneyart.model.dao;
 
-import it.unisa.c02.moneyart.model.beans.*;
+import it.unisa.c02.moneyart.model.beans.Opera;
+import it.unisa.c02.moneyart.model.beans.Rivendita;
 import it.unisa.c02.moneyart.model.dao.interfaces.RivenditaDao;
 import it.unisa.c02.moneyart.utils.production.Retriever;
-import javax.sql.DataSource;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
 import java.util.ArrayList;
 import java.util.List;
+import javax.sql.DataSource;
 
 /**
  * Implementa la classe che esplicita i metodi
@@ -14,220 +19,222 @@ import java.util.List;
  */
 public class RivenditaDaoImpl implements RivenditaDao {
 
-    public RivenditaDaoImpl() {
-        this.ds = (DataSource) Retriever.getIstance(DataSource.class);
+  public RivenditaDaoImpl() {
+    this.ds = (DataSource) Retriever.getIstance(DataSource.class);
+  }
+
+  /**
+   * Costruttore, permette di specificare il datasource utilizzato.
+   *
+   * @param ds il datasource utilizzato
+   */
+  public RivenditaDaoImpl(DataSource ds) {
+    this.ds = ds;
+  }
+
+  /**
+   * Inserisce un item del database.
+   *
+   * @param item l'oggetto da inserire nel database
+   */
+  @Override
+  public void doCreate(Rivendita item) {
+    String sql =
+        "INSERT INTO " + TABLE_NAME
+        + "(id_opera, prezzo, stato) "
+        + " VALUES(?, ? , ?) ";
+
+
+    try (Connection connection = ds.getConnection();
+         PreparedStatement preparedStatement = connection.prepareStatement(sql,
+            PreparedStatement.RETURN_GENERATED_KEYS)) {
+      preparedStatement.setObject(1, item.getOpera().getId(), Types.INTEGER);
+      preparedStatement.setObject(2, item.getPrezzo(), Types.DOUBLE);
+      preparedStatement.setObject(3, item.getStato().toString().toLowerCase());
+      preparedStatement.executeUpdate();
+      ResultSet resultSet = preparedStatement.getGeneratedKeys();
+      if (resultSet != null && resultSet.next()) {
+        item.setId(resultSet.getInt(1));
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw new IllegalArgumentException(e.getMessage());
     }
 
-    /**
-     * Costruttore, permette di specificare il datasource utilizzato.
-     *
-     * @param ds il datasource utilizzato
-     */
-    public RivenditaDaoImpl(DataSource ds) {
-        this.ds = ds;
+
+  }
+
+  /**
+   * Ricerca nel database un item tramite un identificativo univoco.
+   *
+   * @param id l'identificativo dell'item
+   * @return item trovato nel database
+   */
+  @Override
+  public Rivendita doRetrieveById(int id) {
+    String sql =
+        "select * from " + TABLE_NAME
+        + " where id = ? ";
+    Rivendita rivendita = null;
+
+
+    try (Connection connection = ds.getConnection();
+         PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+      preparedStatement.setInt(1, id);
+
+      ResultSet rs = preparedStatement.executeQuery();
+      rivendita = new Rivendita();
+
+      rivendita = getSingleResultFromResultSet(rs);
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw new IllegalArgumentException(e.getMessage());
     }
 
-    /**
-     * Inserisce un item del database.
-     *
-     * @param item l'oggetto da inserire nel database
-     */
-    @Override
-    public void doCreate(Rivendita item) {
-        String insertSQL =
-                "INSERT INTO " + TABLE_NAME +
-                        "(id_opera, prezzo, stato) "
-                        + " VALUES(?, ? , ?) ";
+    return rivendita;
+  }
+
+  /**
+   * Ricerca nel database tutti gli item,
+   * eventualmente ordinati tramite un filtro.
+   *
+   * @param filter filtro di ordinamento delle tuple
+   * @return lista di item trovata nel database
+   */
+  @Override
+  public List<Rivendita> doRetrieveAll(String filter) {
+    String sql =
+        "select * from " + TABLE_NAME;
+    List<Rivendita> rivendite = null;
+
+    if (filter != null && !filter.equals("")) {
+      sql += " ORDER BY " + filter;
+    }
+
+    try (Connection connection = ds.getConnection();
+         PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+      rivendite = new ArrayList<>();
+
+      ResultSet rs = preparedStatement.executeQuery();
+      rivendite = getMultipleResultFromResultSet(rs);
+
+      return rivendite;
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw new IllegalArgumentException(e.getMessage());
+    }
+  }
+
+  /**
+   * Aggiorna l'item bel database.
+   *
+   * @param item l'item da aggiornare
+   */
+  @Override
+  public void doUpdate(Rivendita item) {
+    String sql =
+        "UPDATE " + TABLE_NAME
+        + " set id_opera = ?,prezzo = ?, stato = ?"
+        + " where id = ?";
 
 
-        try (Connection connection = ds.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(insertSQL,
-                     PreparedStatement.RETURN_GENERATED_KEYS)) {
-            preparedStatement.setObject(1, item.getOpera().getId(), Types.INTEGER);
-            preparedStatement.setObject(2, item.getPrezzo(), Types.DOUBLE);
-            preparedStatement.setObject(3, item.getStato().toString().toLowerCase());
-            preparedStatement.executeUpdate();
-            ResultSet resultSet = preparedStatement.getGeneratedKeys();
-            if (resultSet != null && resultSet.next()) {
-                item.setId(resultSet.getInt(1));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new IllegalArgumentException(e.getMessage());
-        }
+    try (Connection connection = ds.getConnection();
+         PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+      preparedStatement.setObject(1, item.getOpera().getId(), Types.INTEGER);
+      preparedStatement.setObject(2, item.getStato().toString().toLowerCase());
+      preparedStatement.setObject(3, item.getPrezzo(), Types.DOUBLE);
+      preparedStatement.setObject(4, item.getId(), Types.INTEGER);
+      preparedStatement.executeUpdate();
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw new IllegalArgumentException(e.getMessage());
+    }
+  }
+
+  /**
+   * Elimina item dal database.
+   *
+   * @param item l'item da eliminare
+   */
+  @Override
+  public void doDelete(Rivendita item) {
+    String sql =
+        "delete from " + TABLE_NAME
+        + " where id = ? ";
+
+
+    try (Connection connection = ds.getConnection();
+         PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+      preparedStatement.setObject(1, item.getId(), Types.INTEGER);
+
+      preparedStatement.executeUpdate();
+
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw new IllegalArgumentException(e.getMessage());
+    }
+  }
+
+  /**
+   * Metodo privato per restituire un singolo oggetto Rivendita dopo aver
+   * effettuato un'interrogazione al db.
+   *
+   * @param rs il ResultSet
+   * @return l'oggetto Segnalazione
+   * @throws SQLException l'eccezione sql lanciata in caso di errore
+   */
+  private Rivendita getSingleResultFromResultSet(ResultSet rs) throws SQLException {
+    Rivendita rivendita = null;
+    if (rs.next()) {
+      rivendita = new Rivendita();
+      rivendita.setId(rs.getObject("id", Integer.class));
+
+      Opera opera = new Opera();
+      opera.setId(rs.getObject("id_utente", Integer.class));
+      rivendita.setOpera(opera);
+
+      rivendita.setOpera(rs.getObject("id_opera", Opera.class));
+      rivendita.setPrezzo(rs.getObject("prezzo", Double.class));
+      rivendita.setStato(Rivendita.Stato.valueOf(rs.getObject("stato", String.class)
+          .toUpperCase()));
 
 
     }
+    return rivendita;
+  }
 
-    /**
-     * Ricerca nel database un item tramite un identificativo univoco.
-     *
-     * @param id l'identificativo dell'item
-     * @return item trovato nel database
-     */
-    @Override
-    public Rivendita doRetrieveById(int id) {
-        String retrieveSQL =
-                "select * from " + TABLE_NAME +
-                        " where id = ? ";
-        Rivendita rivendita = null;
+  /**
+   * Metodo privato per restituire una collezione di oggetti Rivendita
+   * dopo aver effettuato un'interrogazione al db.
+   *
+   * @param rs il ResultSet
+   * @return la collezione di oggetti Segnalazione
+   * @throws SQLException l'eccezione sql lanciata in caso di errore
+   */
+  private List<Rivendita> getMultipleResultFromResultSet(ResultSet rs) throws SQLException {
+    List<Rivendita> rivendite = new ArrayList<>();
+    while (rs.next()) {
+      Rivendita rivendita = new Rivendita();
+      rivendita.setId(rs.getObject("id", Integer.class));
+
+      Opera opera = new Opera();
+      opera.setId(rs.getObject("id_opera", Integer.class));
+      rivendita.setOpera(opera);
+
+      rivendita.setPrezzo(rs.getObject("prezzo", Double.class));
+      rivendita.setStato(Rivendita.Stato.valueOf(rs.getObject("stato", String.class)
+          .toUpperCase()));
+      rivendite.add(rivendita);
 
 
-        try (Connection connection = ds.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(retrieveSQL)) {
-            preparedStatement.setInt(1, id);
-
-            ResultSet rs = preparedStatement.executeQuery();
-            rivendita = new Rivendita();
-
-            rivendita = getSingleResultFromResultSet(rs);
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new IllegalArgumentException(e.getMessage());
-        }
-
-        return rivendita;
     }
+    return rivendite;
+  }
 
-    /**
-     * Ricerca nel database tutti gli item,
-     * eventualmente ordinati tramite un filtro.
-     *
-     * @param filter filtro di ordinamento delle tuple
-     * @return lista di item trovata nel database
-     */
-    @Override
-    public List<Rivendita> doRetrieveAll(String filter) {
-        String retrieveSQL =
-                "select * from " + TABLE_NAME;
-        List<Rivendita> rivendite = null;
-
-        if (filter != null && !filter.equals("")) {
-            retrieveSQL += " ORDER BY " + filter;
-        }
-
-        try (Connection connection = ds.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(retrieveSQL)) {
-            rivendite = new ArrayList<>();
-
-            ResultSet rs = preparedStatement.executeQuery();
-            rivendite = getMultipleResultFromResultSet(rs);
-
-            return rivendite;
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new IllegalArgumentException(e.getMessage());
-        }
-    }
-
-    /**
-     * Aggiorna l'item bel database.
-     *
-     * @param item l'item da aggiornare
-     */
-    @Override
-    public void doUpdate(Rivendita item) {
-        String updateSQL =
-                "UPDATE " + TABLE_NAME +
-                        " set id_opera = ?,prezzo = ?, stato = ?"
-                        + " where id = ?";
-
-
-        try (Connection connection = ds.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(updateSQL)) {
-            preparedStatement.setObject(1, item.getOpera().getId(), Types.INTEGER);
-            preparedStatement.setObject(2, item.getStato().toString().toLowerCase());
-            preparedStatement.setObject(3, item.getPrezzo(), Types.DOUBLE);
-            preparedStatement.setObject(4, item.getId(), Types.INTEGER);
-            preparedStatement.executeUpdate();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new IllegalArgumentException(e.getMessage());
-        }
-    }
-
-    /**
-     * Elimina item dal database.
-     *
-     * @param item l'item da eliminare
-     */
-    @Override
-    public void doDelete(Rivendita item) {
-        String deleteSQL =
-                "delete from " + TABLE_NAME +
-                        " where id = ? ";
-
-
-        try (Connection connection = ds.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(deleteSQL)) {
-            preparedStatement.setObject(1, item.getId(), Types.INTEGER);
-
-            preparedStatement.executeUpdate();
-
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new IllegalArgumentException(e.getMessage());
-        }
-    }
-
-    /**
-     * Metodo privato per restituire un singolo oggetto Rivendita dopo aver
-     * effettuato un'interrogazione al db.
-     *
-     * @param rs il ResultSet
-     * @return l'oggetto Segnalazione
-     * @throws SQLException l'eccezione sql lanciata in caso di errore
-     */
-    private Rivendita getSingleResultFromResultSet(ResultSet rs) throws SQLException {
-        Rivendita rivendita = null;
-        if (rs.next()) {
-            rivendita = new Rivendita();
-            rivendita.setId(rs.getObject("id", Integer.class));
-
-            Opera opera = new Opera();
-            opera.setId(rs.getObject("id_utente", Integer.class));
-            rivendita.setOpera(opera);
-
-            rivendita.setOpera(rs.getObject("id_opera", Opera.class));
-            rivendita.setPrezzo(rs.getObject("prezzo", Double.class));
-            rivendita.setStato(Rivendita.Stato.valueOf(rs.getObject("stato", String.class).toUpperCase()));
-
-
-        }
-        return rivendita;
-    }
-
-    /**
-     * Metodo privato per restituire una collezione di oggetti Rivendita
-     * dopo aver effettuato un'interrogazione al db.
-     *
-     * @param rs il ResultSet
-     * @return la collezione di oggetti Segnalazione
-     * @throws SQLException l'eccezione sql lanciata in caso di errore
-     */
-    private List<Rivendita> getMultipleResultFromResultSet(ResultSet rs) throws SQLException {
-        List<Rivendita> rivendite = new ArrayList<>();
-        while (rs.next()) {
-            Rivendita rivendita = new Rivendita();
-            rivendita.setId(rs.getObject("id", Integer.class));
-
-            Opera opera = new Opera();
-            opera.setId(rs.getObject("id_opera", Integer.class));
-            rivendita.setOpera(opera);
-
-            rivendita.setPrezzo(rs.getObject("prezzo", Double.class));
-            rivendita.setStato(Rivendita.Stato.valueOf(rs.getObject("stato", String.class).toUpperCase()));
-            rivendite.add(rivendita);
-
-
-        }
-        return rivendite;
-    }
-
-    private static DataSource ds;
-    private static final String TABLE_NAME = "rivendita";
+  private static DataSource ds;
+  private static final String TABLE_NAME = "rivendita";
 }
