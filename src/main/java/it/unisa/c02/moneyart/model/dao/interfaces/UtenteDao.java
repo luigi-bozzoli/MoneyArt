a7cb24518a411188c9a561dc426c771d6cc6820c
AurySepe <aureliosepe01@gmail.com>
2022-01-04 12:20:54 +0100

    changed UtenteDao

--- src/main/java/it/unisa/c02/moneyart/model/dao/interfaces/UtenteDao.java
+++ src/main/java/it/unisa/c02/moneyart/model/dao/interfaces/UtenteDao.java
@@ -1,32 +1,28 @@
 package it.unisa.c02.moneyart.model.dao.interfaces;
 
-import it.unisa.c02.moneyart.model.beans.Asta;
-import it.unisa.c02.moneyart.model.beans.Notifica;
-import it.unisa.c02.moneyart.model.beans.Rivendita;
 import it.unisa.c02.moneyart.model.beans.Utente;
-
 import java.util.List;
 
 /**
  * Questa classe rappresenta il DAO di un Utente.
  */
-public interface UtenteDAO extends GenericDao<Utente> {
+public interface UtenteDao extends GenericDao<Utente> {
 //signature metodi aggiuntivi.....
 
-    /**
-     * Restituisce l'utente in base all'username
-     *
-     * @param username l'username dell'utente
-     * @return l'utente con quell'username
-     */
-    Utente doRetrieveByUsername(String username);
+  /**
+   * Restituisce l'utente in base all'username
+   *
+   * @param username l'username dell'utente
+   * @return l'utente con quell'username
+   */
+  Utente doRetrieveByUsername(String username);
 
-    /**
-     * Restituisce tutti gli utenti che seguono utente
-     *
-     * @param utente l'utente di cui vogliamo conoscere i follower
-     * @return i follower di quell'utente
-     */
-    List<Utente> getFollowers (Utente utente);
+  /**
+   * Restituisce tutti gli utenti che seguono uno specifico utente.
+   *
+   * @param id l'identificativo dell'utente di cui vogliamo conoscere i follower
+   * @return i follower di quell'utente
+   */
+  List<Utente> doRetrieveFollowersByUserId(int id);
 
 }
