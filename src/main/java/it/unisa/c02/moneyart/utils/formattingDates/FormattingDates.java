d4fa5280a2847ff1b0371954c05d6157de055b89
shoyll <michaeldesantis1998@gmail.com>
2022-01-09 17:41:27 +0100

    elimination of methods not used in date formatting

--- src/main/java/it/unisa/c02/moneyart/utils/formattingDates/FormattingDates.java
+++ src/main/java/it/unisa/c02/moneyart/utils/formattingDates/FormattingDates.java
@@ -7,29 +7,6 @@ import java.util.Date;
 
 public class FormattingDates {
 
-    /**
-     * Converte una stringa contente una data nel formato "dd/MM/yyyy"
-     * in tipo Date (java.util.Date)
-     * @param dataS la stringa contente una data nel formato "dd/MM/yyyy"
-     * @return la stessa data in formato Date
-     */
-    public static Date fromStringToDate (String dataS) throws ParseException {
-        if (dataS == null) return null;
-
-        return sdf.parse(dataS);
-
-    }
-
-   /**
-    * Converte un tipo Date in una stringa del formato "dd/MM/yyyy"
-    */
-   public static String fromDateToString (Date data) {
-      if (data == null) return null;
-
-      return sdf.format(data);
-
-   }
-
    /**
     * La data viene restituita con il tempo a mezzanotte
     * @param date la data originaria
