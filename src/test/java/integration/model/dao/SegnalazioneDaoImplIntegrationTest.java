244bf243c0746a6b139594a15861975ca4fee8bb
stefanozarro <stefanozarro@gmail.com>
2022-01-15 21:11:29 +0100

    added setup SegnalazioneDaoImplIntegrationTest.java

---
+++ src/test/java/integration/model/dao/SegnalazioneDaoImplIntegrationTest.java
@@ -0,0 +1,79 @@
+package integration.model.dao;
+
+import hthurow.tomcatjndi.TomcatJNDI;
+import it.unisa.c02.moneyart.model.beans.Segnalazione;
+import it.unisa.c02.moneyart.model.dao.AstaDaoImpl;
+import it.unisa.c02.moneyart.model.dao.OperaDaoImpl;
+import it.unisa.c02.moneyart.model.dao.RivenditaDaoImpl;
+import it.unisa.c02.moneyart.model.dao.SegnalazioneDaoImpl;
+import it.unisa.c02.moneyart.model.dao.interfaces.AstaDao;
+import it.unisa.c02.moneyart.model.dao.interfaces.RivenditaDao;
+import it.unisa.c02.moneyart.model.dao.interfaces.SegnalazioneDao;
+import org.apache.ibatis.jdbc.ScriptRunner;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.io.*;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@DisplayName("SegnalazioneDao")
+public class SegnalazioneDaoImplIntegrationTest {
+
+  private static DataSource dataSource;
+
+  private SegnalazioneDao segnalazioneDao;
+  private AstaDao astaDao;
+
+  @BeforeAll
+  static void generalSetUp(){
+    Context initCtx = null;
+    Context envCtx = null;
+    try{
+      initCtx = new InitialContext();
+      envCtx = (Context) initCtx.lookup("java:comp/env");
+      dataSource = (DataSource) envCtx.lookup("jdbc/storage");
+    } catch (NamingException e) {
+      TomcatJNDI tomcatJNDI = new TomcatJNDI();
+      tomcatJNDI.processContextXml(new File("src/main/webapp/META-INF/context.xml"));
+      tomcatJNDI.start();
+    }
+    if(envCtx == null){
+      try{
+        initCtx = new InitialContext();
+        envCtx = (Context) initCtx.lookup("java:comp/env");
+        dataSource = (DataSource) envCtx.lookup("jdbc/storage");
+      } catch (NamingException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  @BeforeEach
+  void setUp() throws SQLException, FileNotFoundException {
+    Connection connection = dataSource.getConnection();
+    segnalazioneDao = new SegnalazioneDaoImpl(dataSource);
+    astaDao = new AstaDaoImpl(dataSource);
+    ScriptRunner runner = new ScriptRunner(connection);
+    runner.setLogWriter(null);
+    Reader reader = new BufferedReader(new FileReader("./src/test/database/populate_all.sql"));
+    runner.runScript(reader);
+    connection.close();
+  }
+
+  @AfterEach
+  void tearDown() throws SQLException, IOException {
+    Connection connection = dataSource.getConnection();
+    ScriptRunner runner = new ScriptRunner(connection);
+    runner.setLogWriter(null);
+    Reader reader = new BufferedReader(new FileReader("./src/test/database/clean_all.sql"));
+    runner.runScript(reader);
+    connection.close();
+  }
+}
