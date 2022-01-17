ca812ff9209933273d6902d48ad8345298b2a0d4
Daniele Galloppo <daniele.galloppo@protonmail.com>
2022-01-17 02:44:11 +0100

    Added initial implementation of NotificaDaoImplIntegrationTest (Integration test for Notifica DAO)

---
+++ src/test/database/clean_database.sql
@@ -0,0 +1,15 @@
+DELETE FROM moneyart.segnalazione WHERE id > 0;
+DELETE FROM moneyart.notifica WHERE id > 0;
+DELETE FROM moneyart.partecipazione WHERE id > 0;
+DELETE FROM moneyart.rivendita WHERE id > 0;
+DELETE FROM moneyart.asta WHERE id > 0;
+DELETE FROM moneyart.opera WHERE id > 0;
+DELETE FROM moneyart.utente WHERE id > 0;
+
+ALTER TABLE moneyart.segnalazione AUTO_INCREMENT = 1;
+ALTER TABLE moneyart.notifica AUTO_INCREMENT = 1;
+ALTER TABLE moneyart.partecipazione AUTO_INCREMENT = 1;
+ALTER TABLE moneyart.rivendita AUTO_INCREMENT = 1;
+ALTER TABLE moneyart.asta AUTO_INCREMENT = 1;
+ALTER TABLE moneyart.opera AUTO_INCREMENT = 1;
+ALTER TABLE moneyart.utente AUTO_INCREMENT = 1;
