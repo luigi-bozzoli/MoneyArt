b99cfb3117ac566dd06a1f904d74da40daae0f62
AurySepe <aureliosepe01@gmail.com>
2022-01-19 00:23:18 +0100

    added timer package unit and integration tests

--- src/test/java/integration/utils/timers/TimedObjectDaoImplIntegrationTest.java
+++ src/test/java/integration/utils/timers/TimedObjectDaoImplIntegrationTest.java
@@ -1,7 +1,6 @@
 package integration.utils.timers;
 
 import hthurow.tomcatjndi.TomcatJNDI;
-import it.unisa.c02.moneyart.model.beans.Utente;
 import it.unisa.c02.moneyart.utils.timers.TimedObject;
 import it.unisa.c02.moneyart.utils.timers.TimedObjectDao;
 import it.unisa.c02.moneyart.utils.timers.TimedObjectDaoImpl;
@@ -9,25 +8,24 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.io.Reader;
-import java.security.MessageDigest;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 import org.apache.ibatis.jdbc.ScriptRunner;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -84,7 +82,7 @@ class TimedObjectDaoImplIntegrationTest {
         throws Exception {
 
       SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-      TimedObject timedObject1 = new TimedObject(100,"avviaAsta",simpleDateFormat.parse("2014-02-11"));
+      TimedObject timedObject1 = new TimedObject(null,"avviaAsta",simpleDateFormat.parse("2014-02-11"));
       TimedObject timedObject2 = new TimedObject("aaaa","TerminaAsta",simpleDateFormat.parse("2014-02-11"));
       TimedObject timedObject3 = new TimedObject(300L,"Prova",simpleDateFormat.parse("2014-02-11"));
 
@@ -92,8 +90,33 @@ class TimedObjectDaoImplIntegrationTest {
     }
   }
 
+  static class ListTimedObjectProvider implements ArgumentsProvider {
+
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext)
+        throws Exception {
+
+      SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+      TimedObject timedObject1 = new TimedObject(100,"avviaAsta",simpleDateFormat.parse("2014-05-11"));
+      TimedObject timedObject2 = new TimedObject("aaaa","TerminaAsta",simpleDateFormat.parse("2019-02-11"));
+      TimedObject timedObject3 = new TimedObject(simpleDateFormat.parse("2019-01-11"),"Attiva Rivendita",simpleDateFormat.parse("2014-02-11"));
+      TimedObject timedObject4 = new TimedObject(100f,"k",simpleDateFormat.parse("2013-02-11"));
+      TimedObject timedObject5 = new TimedObject(300L,"Prova",simpleDateFormat.parse("2015-02-11"));
+
+      List<TimedObject> list1 =
+          Arrays.asList(timedObject1, timedObject2, timedObject3);
+      List<TimedObject> list2 =
+          Arrays.asList(timedObject4, timedObject5);
+      List<TimedObject> list3 = new ArrayList<>();
+
+      return Stream.of(Arguments.of(list1),Arguments.of(list2),Arguments.of(list3));
+
+
+    }
+  }
+
   @ParameterizedTest
-  @DisplayName("create test")
+  @DisplayName("Create")
   @ArgumentsSource(TimedObjectProvider.class)
   void doCreate(TimedObject timedObject) {
     timedObjectDao.doCreate(timedObject);
@@ -101,19 +124,49 @@ class TimedObjectDaoImplIntegrationTest {
     Assertions.assertEquals(timedObject,result);
   }
 
-  @Test
-  void doRetrieveById() {
+  @ParameterizedTest
+  @DisplayName("Retrieve")
+  @ArgumentsSource(TimedObjectProvider.class)
+  void doRetrieveById(TimedObject timedObject) {
+    timedObjectDao.doCreate(timedObject);
+    TimedObject result = timedObjectDao.doRetrieveById(timedObject.getId());
+    Assertions.assertEquals(result,timedObject);
   }
 
-  @Test
-  void doRetrieveAll() {
+  @ParameterizedTest
+  @DisplayName("Retrieve All")
+  @ArgumentsSource(ListTimedObjectProvider.class)
+  void doRetrieveAll(List<TimedObject> list) {
+    for (TimedObject t : list){
+      timedObjectDao.doCreate(t);
+    }
+    List<TimedObject> result = timedObjectDao.doRetrieveAll("id");
+    Assertions.assertArrayEquals(list.toArray(),result.toArray());
   }
 
-  @Test
-  void doUpdate() {
+  @ParameterizedTest
+  @DisplayName("Update")
+  @ArgumentsSource(TimedObjectProvider.class)
+  void doUpdate(TimedObject timedObject) throws ParseException {
+    timedObjectDao.doCreate(timedObject);
+    TimedObject toUpdate = timedObjectDao.doRetrieveById(timedObject.getId());
+    toUpdate.setAttribute("cambiamento");
+    toUpdate.setTaskType("modifica");
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    toUpdate.setTaskDate(simpleDateFormat.parse("2020-11-10"));
+    timedObjectDao.doUpdate(toUpdate);
+    TimedObject result = timedObjectDao.doRetrieveById(toUpdate.getId());
+    Assertions.assertEquals(toUpdate,result);
+
   }
 
-  @Test
-  void doDelete() {
+  @ParameterizedTest
+  @DisplayName("Delete")
+  @ArgumentsSource(TimedObjectProvider.class)
+  void doDelete(TimedObject timedObject) {
+    timedObjectDao.doCreate(timedObject);
+    Assertions.assertNotNull(timedObjectDao.doRetrieveById(timedObject.getId()));
+    timedObjectDao.doDelete(timedObject);
+    Assertions.assertNull(timedObjectDao.doRetrieveById(timedObject.getId()));
   }
 }
