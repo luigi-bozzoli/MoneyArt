dab507e1733067c7e8ac8829f4808edc14d09588
shoyll <michaeldesantis1998@gmail.com>
2022-01-19 21:33:21 +0100

    doRetrieveByIdCatch + doRetrieveAll + doRetrieveAllCatch added (UtenteDaoImplUnitTest)

--- src/test/java/unit/model/dao/UtenteDaoImplUnitTest.java
+++ src/test/java/unit/model/dao/UtenteDaoImplUnitTest.java
@@ -6,6 +6,7 @@ import it.unisa.c02.moneyart.model.dao.OperaDaoImpl;
 import it.unisa.c02.moneyart.model.dao.UtenteDaoImpl;
 
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
@@ -15,6 +16,8 @@ import static org.mockito.Mockito.when;
 
 import javax.sql.DataSource;
 import java.sql.*;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -124,20 +127,70 @@ public class UtenteDaoImplUnitTest {
     assertTrue(user.getId() == result.getId());
   }
 
+  @Test
+  @DisplayName("doRetrieveByIdCatch")
+  void doRetrieveByIdCatch() throws SQLException {
+
+    when(connection.prepareStatement(anyString())).thenThrow(SQLException.class);
+
+    Utente userResult = new UtenteDaoImpl(dataSource).doRetrieveById(user.getId());
+    assertNull(userResult);
+  }
+
+  @Test
+  void doRetrieveAll() throws SQLException {
+
+    //costruisco la lista oracolo del nostro test unit
+    Utente us1 = user;
+    Utente us2 = new Utente("Stefano", "Zarro", null, "stefanus@unisa.it",
+            "s_ano", user, new byte[10], 0.002);
+    user.setId(user.getId()+50);
+
+    List<Utente> utentiOracolo = Arrays.asList(us1, us2);
+
+    /*Istruisco i mock di connessione per questo metodo +
+    Istruisco il finto comportamento di prelevazione dell'opera dal db
+    */
+
+    when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
+    doNothing().when(preparedStatement).setInt(anyInt(), anyInt());
+    when(resultSet.next()).thenReturn(Boolean.TRUE, Boolean.TRUE, Boolean.FALSE);
+    when(preparedStatement.executeQuery()).thenReturn(resultSet);
+    when(preparedStatement.executeQuery(anyString())).thenReturn(resultSet);
+
+    when(resultSet.getObject("id", Integer.class)).thenReturn(us1.getId(), us2.getId());
+    when(resultSet.getObject("id_seguito", Integer.class)).thenReturn(us1.getSeguito().getId(), us2.getSeguito().getId());
+    when(resultSet.getObject("email", String.class)).thenReturn(us1.getEmail(), us2.getEmail());
+    when(resultSet.getObject("pwd", String.class)).thenReturn(us1.getPassword().toString(), us2.getPassword().toString());
+    when(resultSet.getObject("username", String.class)).thenReturn(us1.getUsername(), us2.getUsername());
+    when(resultSet.getObject("nome", String.class)).thenReturn(us1.getNome(), us2.getNome());
+    when(resultSet.getObject("cognome", String.class)).thenReturn(us1.getCognome(), us2.getCognome());
+    when(resultSet.getObject("foto", Blob.class)).thenReturn(us1.getFotoProfilo(), us2.getFotoProfilo());
+    when(resultSet.getObject("saldo", Double.class)).thenReturn(us1.getSaldo(), us2.getSaldo());
+
+
+    List<Utente> utentiRetrieve = new UtenteDaoImpl(dataSource).doRetrieveAll("id");
+
+    System.out.println(utentiOracolo);
+    System.out.println(utentiRetrieve);
+
+    assertTrue(utentiOracolo.get(0).getId() == utentiRetrieve.get(0).getId() && utentiOracolo.get(1).getId() == utentiRetrieve.get(1).getId());
+
+  }
+
+
+  @Test
+  @DisplayName("doRetrieveAllCatch")
+  void doRetrieveAllCatch() throws SQLException {
+
+    when(connection.prepareStatement(anyString())).thenThrow(SQLException.class);
+
+    UtenteDaoImpl userDao = new UtenteDaoImpl(dataSource);
+    List<Utente> users = userDao.doRetrieveAll("id");
+    assertNull(users);
+  }
 
 /*
-  @Test
-  void doRetrieveAll() {
-  }
-
-  @Test
-  void doUpdate() {
-  }
-
-  @Test
-  void doDelete() {
-  }
-
   @Test
   void doRetrieveByUsername() {
   }
