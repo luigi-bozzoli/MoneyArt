7ab5e5f0bd16b3b9bc83d4ab61fc4af952af1c20
shoyll <michaeldesantis1998@gmail.com>
2022-01-19 18:52:37 +0100

    doRetrieveAll and doRetrieveAllCatch (OperaDaoImplUnitTest's)

--- src/test/java/unit/model/dao/OperaDaoImplUnitTest.java
+++ src/test/java/unit/model/dao/OperaDaoImplUnitTest.java
@@ -1,5 +1,6 @@
 package unit.model.dao;
 
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
@@ -9,6 +10,8 @@ import it.unisa.c02.moneyart.model.beans.Utente;
 import it.unisa.c02.moneyart.model.dao.OperaDaoImpl;
 
 import java.sql.*;
+import java.util.Arrays;
+import java.util.List;
 import javax.sql.DataSource;
 
 import org.junit.jupiter.api.*;
@@ -148,16 +151,83 @@ public class OperaDaoImplUnitTest {
 
     Opera opResult = new OperaDaoImpl(dataSource).doRetrieveById(op.getId());
 
-
     assertTrue(op.getId() == opResult.getId());
   }
 
-  /*
 
   @Test
-  void doRetrieveAll() {
+  @DisplayName("doRetrieveByIdCatch")
+  void doRetrieveByIdCatch() throws SQLException {
+    SQLException ex = new SQLException();
+    when(connection.prepareStatement(anyString())).thenThrow(ex);
+
+    OperaDaoImpl operaDao = new OperaDaoImpl(dataSource);
+    Opera opera = operaDao.doRetrieveById(90);
+    assertNull(opera);
   }
 
+
+
+  @Test
+  void doRetrieveAll() throws SQLException {
+
+    //Dipendenze bean
+    Utente userFollowed = new Utente("MarioVip", "RossiVip", opera.getImmagine(), "mariorossivip@unisa.it",
+            "m_red_vip", null, new byte[10], 2000000.2);
+    userFollowed.setId(99);
+
+    Utente user = new Utente("Mario", "Rossi", opera.getImmagine(), "mariorossi@unisa.it",
+            "m_red", userFollowed, new byte[10], 2.2);
+    user.setId(possessore.getId());
+
+    Utente artist = new Utente("Nick", "Arte", opera.getImmagine(), "nickarte@unisa.it",
+            "n_art", userFollowed, new byte[10], 5000.2);
+    artist.setId(artista.getId());
+
+    //*********************************************************************************
+
+    //costruisco la lista oracolo del nostro test unit
+    Opera op1 = new Opera(opera.getNome(), opera.getDescrizione(), opera.getStato(),
+            opera.getImmagine(), user, artist, opera.getCertificato());
+    op1.setId(opera.getId());
+
+    Opera op2 = new Opera("opera2", opera.getDescrizione(), opera.getStato(),
+            opera.getImmagine(), artist, userFollowed, opera.getCertificato());
+    op2.setId(opera.getId()+1);
+
+    List<Opera> opereOracolo = Arrays.asList(op1, op2);
+
+    /*Istruisco i mock di connessione per questo metodo +
+    Istruisco il finto comportamento di prelevazione dell'opera dal db
+    */
+
+    when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
+    doNothing().when(preparedStatement).setInt(anyInt(), anyInt());
+    when(resultSet.next()).thenReturn(Boolean.TRUE, Boolean.TRUE, Boolean.FALSE);
+    when(preparedStatement.executeQuery()).thenReturn(resultSet);
+    when(preparedStatement.executeQuery(anyString())).thenReturn(resultSet);
+
+    when(resultSet.getObject("id", Integer.class)).thenReturn(op1.getId(), op2.getId());
+    when(resultSet.getObject("id_utente", Integer.class)).thenReturn(op1.getPossessore().getId(), op2.getPossessore().getId());
+    when(resultSet.getObject("id_artista", Integer.class)).thenReturn(op1.getArtista().getId(), op2.getArtista().getId());
+    when(resultSet.getObject("nome", String.class)).thenReturn(op1.getNome(), op2.getNome());
+    when(resultSet.getObject("descrizione", String.class)).thenReturn(op1.getDescrizione(), op2.getDescrizione());
+    when(resultSet.getObject("immagine", Blob.class)).thenReturn(op1.getImmagine(), op2.getImmagine());
+    when(resultSet.getObject("certificato", String.class)).thenReturn(op1.getCertificato(), op2.getCertificato());
+    when(resultSet.getObject("stato", String.class)).thenReturn(op1.getStato().toString(), op2.getStato().toString());
+
+
+    List<Opera> opereRetrieve = new OperaDaoImpl(dataSource).doRetrieveAll("id");
+
+    System.out.println(opereOracolo);
+    System.out.println(opereRetrieve);
+
+    assertTrue(opereOracolo.get(0).getId()==opereRetrieve.get(0).getId() && opereOracolo.get(1).getId()==opereRetrieve.get(1).getId());
+
+  }
+
+
+/*
   @Test
   void doUpdate() {
   }
