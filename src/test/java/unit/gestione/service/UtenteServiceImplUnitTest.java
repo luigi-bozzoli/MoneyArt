4df71eb0a0f94154c96dad23e70f2e47476c1d21
shoyll <michaeldesantis1998@gmail.com>
2022-01-21 14:51:42 +0100

    redesign structure of OperaDaoImplIntegrationTest

--- src/test/java/unit/gestione/service/UtenteServiceImplUnitTest.java
+++ src/test/java/unit/gestione/service/UtenteServiceImplUnitTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.Arguments; 
 import org.junit.jupiter.params.provider.ArgumentsProvider;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.mockito.Mock;
